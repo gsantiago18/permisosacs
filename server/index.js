f2ce368d252ef0f24e7962852b3524c46757983e
modulo_roles <81981786+gsantiago18@users.noreply.github.com>
2023-10-22 19:57:30 -0500

    modificando querys

--- server/index.js
+++ server/index.js
@@ -14,11 +14,12 @@ const db = mysql.createConnection({
 });
 
 // Ruta para insertar datos en la tabla permisos
-app.post("/create", (req,res)=>{
-  const rol = req.body.rol;
-  const descripcion = req.body.descripcion
+app.post("/insertar-permiso", (req,res)=>{
+  const id_rol=req.body.id_rol
+  const nombre_rol= req.body.nombre_rol
+  const modulo_id= req.bodymodulo_id
 
-  db.query('INSERT INTO roles(nombre_rol, descripcion_rol) VALUES(?,?)',[rol,descripcion],
+  db.query('INSERT INTO permisos(id_rol, nombre_rol,modulo_id) VALUES(?,?,?)',[id_rol,nombre_rol,modulo_id],
   (err,result)=>{
       if(err){
           console.log(err);
