166d31f9142bb90ce9e37a56e595ce8572ffb7c2
modulo_roles <81981786+gsantiago18@users.noreply.github.com>
2023-11-06 17:05:53 -0500

    agregando boton listar

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const express  =require('express');
+const express = require('express');
 const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
@@ -7,28 +7,43 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host:"localhost",
+    host: "localhost",
     user: "root",
-    password : "",
+    password: "",
     database: "gestor_documental"
 });
 
-// Ruta para insertar datos en la tabla permisos
-app.post("/insertar-permiso", (req,res)=>{
-  const id_rol=req.body.id_rol
-  const nombre_rol= req.body.nombre_rol
-  const modulo_id= req.bodymodulo_id
+app.post("/insertar-permiso", (req, res) => {
+    const id_rol = req.body.id_rol;
+    const nombre_rol = req.body.nombre_rol;
+    const modulo_id = req.body.modulo_id; 
 
-  db.query('INSERT INTO permisos(id_rol, nombre_rol,modulo_id) VALUES(?,?,?)',[id_rol,nombre_rol,modulo_id],
-  (err,result)=>{
-      if(err){
-          console.log(err);
-      }else{
-          res.send("rol registrado con éxito")
-      }
-  }
-  );
+    db.query('INSERT INTO permisos (id_rol, nombre_rol, modulo_id) VALUES (?, ?, ?)', [id_rol, nombre_rol, modulo_id],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send("Error al insertar permiso en la base de datos");
+            } else {
+                res.status(200).send("Permiso registrado con éxito");
+            }
+        }
+    );
 });
+
+
+app.get("/permisos", (req, res) => {
+    db.query('    SELECT  permisos.per_id, permisos.id_rol, permisos.nombre_rol, modulos.modulo_nombre FROM permisos  , modulos WHERE  permisos.modulo_id = modulos.modulo_id', (err, result) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Error interno del servidor");
+        } else {
+            res.send(result);
+        }
+    }
+    );
+});
+
+
 app.listen(3001, () => {
-  console.log("Servidor escuchando en el puerto 3001");
+    console.log("Servidor escuchando en el puerto 3001");
 });
